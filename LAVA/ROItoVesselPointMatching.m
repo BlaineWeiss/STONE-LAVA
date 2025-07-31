ba8c1f91a78d744a25efa972112cd7cffb7b7499
Blaine Weiss <blaine.weiss@uky.edu>
2025-07-31 12:08:54 -0400

    Update ROI to Vessel Matching

--- LAVA/ROItoVesselPointMatching.m
+++ LAVA/ROItoVesselPointMatching.m
@@ -13,21 +13,39 @@ function [vertices2,refradvert,xxx,yyy] = ROItoVesselPointMatching(ROI,refrad)
 %
 % License terms: See LICENSE_ACADEMIC.txt and LICENSE_COMMERCIAL.txt for details.
 %
-% For commercial licensing inquiries, contact: blaine.weiss@uky.edu
-
+% For commercial licensing inquiries, contact: [Your Email] or [Tech Transfer Office Email]
+%
 % THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
 
 % If you use this software in published work, please cite:
-
-% Disrupted calcium dynamics in reactive astrocytes occur with endfeet-arteriole decoupling in an amyloid mouse model of Alzheimer’s disease
-% Blaine E. Weiss, et al. bioRxiv 2025.01.24.634584; doi: https://doi.org/10.1101/2025.01.24.634584
+% [Your Citation Here]
 %%
 vertices = round(ROI.Position);
 numpointsvert = length(vertices);
-xxx = interp(vertices(:,1),2);
-yyy = interp(vertices(:,2),2);
 
-vertices2 = [xxx,yyy];
+%xxx = interp(vertices(:,1),2);
+%yyy = interp(vertices(:,2),2);
+
+
+vertices = ROI.Position;  % Nx2 matrix: [x, y]
+vertices(end+1,:) = vertices(1,:);  % Close the polygon
+
+% Compute cumulative distance along the polygon boundary
+dists = [0; cumsum(sqrt(sum(diff(vertices).^2, 2)))];
+
+% Total boundary length and how many points you want
+totalLength = dists(end);
+numInterpPoints = round(size(vertices,1) * 3);  % e.g. triple the number of points
+
+% New equally spaced distance values
+distsInterp = linspace(0, totalLength, numInterpPoints);
+
+% Interpolate X and Y separately using distance as param
+xxx = interp1(dists, vertices(:,1), distsInterp, 'linear');
+yyy = interp1(dists, vertices(:,2), distsInterp, 'linear');
+
+
+vertices2 = [xxx',yyy'];
 vertices2 = round(vertices2);
 vertices2(vertices2 < 1) = 1;
 yyy = vertices2(:,2);
